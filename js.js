9c3659767f11097064db7bf25cfbb8c52ac22f6e
Joshua <Doerak1aug@gmail.com>
2020-01-08 14:41:08 +0100

    .

---
+++ js.js
@@ -0,0 +1,47 @@
+
+var display = 1
+
+
+
+
+function function1()
+{
+    document.getElementById("slider").style.transform = "translateX(0px)"
+    document.getElementById("text1").innerHTML = "Begin nu!"
+    document.getElementById("text2").innerHTML = "Of anders"
+    document.getElementById("text1").style.transform = "translateX(0px)"
+    document.getElementById("text2").style.transform = "translateX(0px)"
+    document.getElementById("moveme").style.transform = "translateX(0px)"
+    document.getElementById("mainbalk").style.transform = "translateX(0px)"
+
+    document.getElementById("button1").style.backgroundColor = "rgb(235, 235, 235)"
+    document.getElementById("button2").style.backgroundColor = "rgb(163, 163, 163)"
+    document.getElementById("button3").style.backgroundColor = "rgb(163, 163, 163)"
+    /* document.getElementsByName("*").style.transform =  */
+}
+ function function2()
+{
+    document.getElementById("slider").style.transform = "translateX(-33.33%)"
+    document.getElementById("text1").innerHTML = "Dit kan jij zijn!"
+    document.getElementById("text2").innerHTML = "alstublieft"
+    document.getElementById("text1").style.transform = "translateX(0px)"
+    document.getElementById("text2").style.transform = "translateX(0px)"
+    document.getElementById("moveme").style.transform = "translateX(0px)"
+
+    document.getElementById("button1").style.backgroundColor = "rgb(163, 163, 163)"
+    document.getElementById("button2").style.backgroundColor = "rgb(235, 235, 235)"
+    document.getElementById("button3").style.backgroundColor = "rgb(196, 196, 196)"
+}
+function function3()
+{
+    document.getElementById("slider").style.transform = "translateX(-66.66%)"
+    document.getElementById("text1").innerHTML = "Dit kan jij ook zijn!"
+    document.getElementById("text2").innerHTML = "haha"
+    document.getElementById("text1").style.transform = "translateX(0px)"
+    document.getElementById("text2").style.transform = "translateX(0px)"
+    document.getElementById("moveme").style.transform = "translateX(0px)"
+
+    document.getElementById("button1").style.backgroundColor = "rgb(163, 163, 163)"
+    document.getElementById("button2").style.backgroundColor = "rgb(163, 163, 163)"
+    document.getElementById("button3").style.backgroundColor = "rgb(235, 235, 235)"
+}
